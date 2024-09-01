bc27dfa16955788f5f671cf0b72e6834e0637df3
ronninxDD <nischalchy15@gmail.com>
2024-09-01 20:46:08 +0545

    Modle and admin created

--- TDL/todo/models.py
+++ TDL/todo/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     user = models.ForeignKey( User, on_delete=models.CASCADE, null = True , blank = True)#links task to a user
     title = models.CharField(max_length = 200,null = True)
     description = models.TextField(null = True,blank = True)
-    completed = models.BooleanField(default = False)
+    complete = models.BooleanField(default = False)
     created = models.DateTimeField(auto_now_add = True)
 
 
